c545870dd8d61f80b64109d55375afa93479e8d5
Mohri Samuel Muakpo <sammuaks@gmail.com>
2025-12-10 20:47:24 +0100

    Update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   reactCompiler: true,
-// };
-
-// export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     domains: ["localhost"],
-//     remotePatterns: [
-//       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         port: "4000",
-//         pathname: "/uploads/**",
-//       },
-//     ],
-//   },
-// };
-
-// module.exports = nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -37,7 +10,7 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'your-production-domain.com', // Replace with your actual domain
+        hostname: 'https://backend-gbenga.onrender.com', // Replace with your actual domain
         pathname: '/uploads/**',
       },
     ],
